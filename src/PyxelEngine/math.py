32a3bea879803fd7d20c2693586e38a63417711c
s316353 <rjsmith3@aep.com>
2022-03-15 14:39:56 -0400

    Formatting

--- src/PyxelEngine/math.py
+++ src/PyxelEngine/math.py
@@ -88,83 +88,88 @@ class Vector2c(ABC):
 class Vector2(Vector2c, np.ndarray):
     def __new__(cls, *data, dtype: Type[DType] = float):
         return np.array(to_tuple2(data), dtype=dtype).view(cls)
-    
+
     def __eq__(self, other: Vector2Like) -> bool:
         return np.all(super().__eq__(other))
-    
+
     def __ne__(self, other: Vector2Like) -> bool:
         return not self.__eq__(other)
-    
+
     @property
     def x(self) -> DType:
         return self[0]
-    
+
     @x.setter
     def x(self, value: DType):
         self[0] = value
-    
+
     @property
     def y(self) -> DType:
         return self[1]
-    
+
     @y.setter
     def y(self, value: DType):
         self[1] = value
-    
+
     @property
     def magnitude(self) -> DType:
         return np.linalg.norm(self)
-    
+
     @magnitude.setter
     def magnitude(self, value: DType):
         self.__imul__(value / self.magnitude)
-    
+
     # noinspection PyTypeChecker
     @property
     def magnitude_sq(self) -> DType:
         return np.dot(self, self)
-    
+
     def normalize(self) -> Vector2:
         return self / self.magnitude
-    
+
     def normalize_self(self) -> Vector2:
         return self.__itruediv__(self.magnitude)
-    
+
     def perpendicular(self) -> Vector2:
         return Vector2(self.y, -self.x)
-    
+
     def perpendicular_self(self) -> Vector2:
         self[:] = self.y, -self.x
         return self
-    
+
     # noinspection PyTypeChecker
     def dot(self, other: Vector2Like) -> DType:
         return np.dot(self, other)
-    
+
     # noinspection PyTypeChecker
     def angle(self) -> float:
         return np.angle(self.x + self.y * 1j)
-    
+
     def angle_between(self, other: Vector2Like) -> float:
         dot: float = self.x * other[0] + self.y * other[1]
         det: float = self.x * other[1] - self.y * other[0]
         return np.arctan2(det, dot)
-    
+
     def distance(self, other: Vector2Like) -> float:
         return np.linalg.norm(self - other)
-    
+
     def distance_sq(self, other: Vector2Like) -> float:
         dx: float = self.x - other[0]
         dy: float = self.y - other[1]
         return dx * dx + dy * dy
-    
+
     def lerp(self, other: Vector2Like, t: float) -> Vector2:
         return (other - self) * t + self
-    
+
     def smooth_step(self, other: Vector2Like, t: float) -> Vector2:
         t2 = t * t
         t3 = t2 * t
-        return (self + self - other - other) * t3 + (3.0 * other - 3.0 * self) * t2 + self * t + self
+        return (
+            (self + self - other - other) * t3
+            + (3.0 * other - 3.0 * self) * t2
+            + self * t
+            + self
+        )
 
 
 class Vector3c(ABC):
@@ -175,63 +180,63 @@ class Vector3c(ABC):
 class Vector3(Vector3c, np.ndarray):
     def __new__(cls, *data, dtype: Type[DType] = float):
         return np.array(to_tuple3(data), dtype=dtype).view(cls)
-    
+
     def __eq__(self, other: Vector3Like) -> bool:
         return np.all(super().__eq__(other))
-    
+
     def __ne__(self, other: Vector3Like) -> bool:
         return not self.__eq__(other)
-    
+
     @property
     def x(self) -> DType:
         return self[0]
-    
+
     @x.setter
     def x(self, value: DType):
         self[0] = value
-    
+
     @property
     def y(self) -> DType:
         return self[1]
-    
+
     @y.setter
     def y(self, value: DType):
         self[1] = value
-    
+
     @property
     def z(self) -> DType:
         return self[2]
-    
+
     @z.setter
     def z(self, value: DType):
         self[2] = value
-    
+
     @property
     def magnitude(self) -> DType:
         return np.linalg.norm(self)
-    
+
     @magnitude.setter
     def magnitude(self, value: DType):
         self.__imul__(value / self.magnitude)
-    
+
     # noinspection PyTypeChecker
     @property
     def magnitude_sq(self) -> DType:
         return np.dot(self, self)
-    
+
     def normalize(self) -> Vector3:
         return self / self.magnitude
-    
+
     def normalize_self(self) -> Vector3:
         return self.__itruediv__(self.magnitude)
-    
+
     # noinspection PyTypeChecker
     def dot(self, other: Vector3Like) -> DType:
         return np.dot(self, other)
-    
+
     def cross(self, other: Vector3Like):
         return np.cross(self, other).view(Vector3)
-    
+
     def angle_between(self, other: Vector3Like) -> float:
         x = self.x
         y = self.y
@@ -245,22 +250,27 @@ class Vector3(Vector3c, np.ndarray):
         cos = cos if cos < 1 else 1
         cos = cos if cos > -1 else -1
         return np.acos(cos)
-    
+
     def distance(self, other: Vector3Like) -> float:
         return np.linalg.norm(self - other)
-    
+
     def distance_sq(self, other: Vector3Like) -> float:
         dx: float = self.x - other[0]
         dy: float = self.y - other[1]
         return dx * dx + dy * dy
-    
+
     def lerp(self, other: Vector3Like, t: float) -> Vector3:
         return (other - self) * t + self
-    
+
     def smooth_step(self, other: Vector2Like, t: float) -> Vector2:
         t2 = t * t
         t3 = t2 * t
-        return (self + self - other - other) * t3 + (3.0 * other - 3.0 * self) * t2 + self * t + self
+        return (
+            (self + self - other - other) * t3
+            + (3.0 * other - 3.0 * self) * t2
+            + self * t
+            + self
+        )
 
 
 class Vector4c(ABC):
@@ -271,71 +281,71 @@ class Vector4c(ABC):
 class Vector4(Vector4c, np.ndarray):
     def __new__(cls, *data, dtype: Type[DType] = float):
         return np.array(to_tuple4(data), dtype=dtype).view(cls)
-    
+
     def __eq__(self, other: Vector4Like) -> bool:
         return np.all(super().__eq__(other))
-    
+
     def __ne__(self, other: Vector4Like) -> bool:
         return not self.__eq__(other)
-    
+
     @property
     def x(self) -> DType:
         return self[0]
-    
+
     @x.setter
     def x(self, value: DType):
         self[0] = value
-    
+
     @property
     def y(self) -> DType:
         return self[1]
-    
+
     @y.setter
     def y(self, value: DType):
         self[1] = value
-    
+
     @property
     def z(self) -> DType:
         return self[2]
-    
+
     @z.setter
     def z(self, value: DType):
         self[2] = value
-    
+
     @property
     def w(self) -> DType:
         return self[3]
-    
+
     @w.setter
     def w(self, value: DType):
         self[3] = value
-    
+
     @property
     def magnitude(self) -> DType:
         return np.linalg.norm(self)
-    
+
     @magnitude.setter
     def magnitude(self, value: DType):
         self.__imul__(value / self.magnitude)
-    
+
     # noinspection PyTypeChecker
     @property
     def magnitude_sq(self) -> DType:
         return np.dot(self, self)
-    
+
     def normalize(self) -> Vector4:
         return self / self.magnitude
-    
+
     def normalize_self(self) -> Vector4:
         return self.__itruediv__(self.magnitude)
-    
+
     # noinspection PyTypeChecker
     def dot(self, other: Vector4Like) -> DType:
         return np.dot(self, other)
-    
+
     def cross(self, other: Vector4Like):
         return np.cross(self, other).view(Vector4)
-    
+
     def angle_between(self, other: Vector4Like) -> float:
         x = self.x
         y = self.y
@@ -343,29 +353,36 @@ class Vector4(Vector4c, np.ndarray):
         w = self.w
         length1Squared: float = x * x + (y * y + (z * z + (w * w)))
         v = to_tuple4(other)
-        length2Squared: float = v[0] * v[0] + (v[1] * v[1] + (v[2] * v[2] + (v[3] * v[3])))
+        length2Squared: float = v[0] * v[0] + (
+            v[1] * v[1] + (v[2] * v[2] + (v[3] * v[3]))
+        )
         dot: float = x * v[0] + (y * v[1] + (z * v[2] + (w * v[3])))
         cos: float = dot / np.sqrt(length1Squared * length2Squared)
         # This is because sometimes cos goes above 1 or below -1 because of lost precision
         cos = cos if cos < 1 else 1
         cos = cos if cos > -1 else -1
         return np.acos(cos)
-    
+
     def distance(self, other: Vector4Like) -> float:
         return np.linalg.norm(self - other)
-    
+
     def distance_sq(self, other: Vector4Like) -> float:
         dx: float = self.x - other[0]
         dy: float = self.y - other[1]
         return dx * dx + dy * dy
-    
+
     def lerp(self, other: Vector4Like, t: float) -> Vector4:
         return (other - self) * t + self
-    
+
     def smooth_step(self, other: Vector2Like, t: float) -> Vector2:
         t2 = t * t
         t3 = t2 * t
-        return (self + self - other - other) * t3 + (3.0 * other - 3.0 * self) * t2 + self * t + self
+        return (
+            (self + self - other - other) * t3
+            + (3.0 * other - 3.0 * self) * t2
+            + self * t
+            + self
+        )
 
 
 Vector2Like = Union[Vector2c, np.ndarray, DType, Iterable[DType]]
